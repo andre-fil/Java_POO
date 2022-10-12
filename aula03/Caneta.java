8c73915a0a4936f191ffa64a0fb0c3e6cbaf9c55
André Barreto <barreto.andre@discente.ufma.br>
2022-10-11 23:26:25 -0300

    getters and setters

---
+++ aula03/Caneta.java
@@ -0,0 +1,33 @@
+package aula03;
+
+public class Caneta {
+
+    public String modelo;
+    private float ponta;
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+
+    public float getPonta(){
+        return this.ponta;
+    }
+
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+    public Caneta(String m ){
+        this.modelo = m;
+        this.ponta = 0.3f;
+    }
+
+    public void status(){
+        System.out.println("Informações");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+    }
+}
---
+++ aula03/Main.java
@@ -0,0 +1,13 @@
+package aula03;
+
+public class Main {
+    public static void main(String[] args){
+        Caneta c1 = new Caneta("Azulão");
+        c1.setPonta((0.6f));
+        c1.status();
+
+
+
+
+    }
+}
