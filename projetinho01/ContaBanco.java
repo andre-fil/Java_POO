a67688c649f97e14fb02b12ecf3bd477674412ae
André Barreto <barreto.andre@discente.ufma.br>
2022-10-13 14:50:40 -0300

    projetinho01 finalizado

--- projetinho01/ContaBanco.java
+++ projetinho01/ContaBanco.java
@@ -8,23 +8,72 @@ public class ContaBanco {
     private float saldo;
     private boolean status;
 
-    public void abrirConta() {
+    public void Estado(){
+        System.out.println("----------------------------------------");
+        System.out.println("Número de conta: " + this.getNumConta());
+        System.out.println("Tipo de conta: " + this.getTipo());
+        System.out.println("Proprietário da conta: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.isStatus());
+        System.out.println("----------------------------------------");
+    }
 
+    public void abrirConta(String tipo_conta){
+        this.setTipo(tipo_conta);
+        this.setStatus(true);
+        if(this.getTipo().equals("cc")){
+            this.setSaldo(50);
+        } else if(this.getTipo().equals("cp")){
+            this.setSaldo(150);
+
+        }
     }
 
     public void fecharConta(){
+        if(this.getSaldo() > 0){
+            System.out.println("Impossível fechar conta! Conta com saldo");
+        } else if (this.getSaldo() < 0){
+            System.out.println("Impossível fechar conta! Conta em débito");
+        } else{
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!");
+        }
 
     }
 
-    public void depositar(){
+    public void depositar(float v){
+        if(this.isStatus()){
+            this.setSaldo(this.getSaldo() + v);
+        } else{
+            System.out.println("Impossível depositar valor");
+        }
 
     }
 
-    public void sacar(){
+    public void sacar(float v){
+        if(this.isStatus() && this.getSaldo() >= v){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Saque realizado com sucesso");
+        } else{
+            System.out.println("Operação de saque indisponível");
+        }
 
     }
 
     public void pagarMensal(){
+       float v;
+        if (this.getTipo().equals("cc")) {
+            v = 12;
+        } else{
+            v = 20;
+        }
+        if(this.isStatus() && this.getSaldo() >= v){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade descontada de saldo!");
+        } else{
+            System.out.println("Não foi possível descontar mensalidade");
+        }
+
 
     }
 
@@ -68,8 +117,16 @@ public class ContaBanco {
         this.status = status;
     }
 
-    public ContaBanco(float saldo, boolean status) {
-        this.saldo = 0;
-        this.status = false;
+    public ContaBanco() {
+        this.setSaldo(0);
+        this.setStatus(false);
     }
+
+
+
+
+
+
+
+
 }
---
+++ projetinho01/Main.java
@@ -0,0 +1,28 @@
+package projetinho01;
+
+public class Main {
+    public static void main(String[] args){
+        ContaBanco c1 = new ContaBanco();
+        c1.setNumConta(80101);
+        c1.setDono("André Barreto");
+        c1.abrirConta("cc");
+        c1.depositar(200);
+        c1.sacar(100);
+        c1.sacar(150);
+        c1.pagarMensal();
+        c1.fecharConta();
+
+        ContaBanco c2 = new ContaBanco();
+        c2.setNumConta(70029);
+        c2.setDono("Lays Eloi");
+        c2.abrirConta("cp");
+        c2.depositar(300);
+        c2.pagarMensal();
+        c2.sacar(430);
+        c2.fecharConta();
+        
+
+        c1.Estado();
+        c2.Estado();
+    }
+}
