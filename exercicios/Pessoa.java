8c588c58b202211430bec1eab262ac1724e22338
André Barreto <barreto.andre@discente.ufma.br>
2022-10-24 10:41:31 -0300

    inicio exercicio

---
+++ exercicios/Pessoa.java
@@ -0,0 +1,37 @@
+package exercicios;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    //m[etodos especiais
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    //others métodos
+    public void fazerAniver(){}
+
+}
