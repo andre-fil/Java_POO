22d6736a9da9b555b9ff5a8f0ddc8e7122ec84c8
André Barreto <barreto.andre@discente.ufma.br>
2022-11-12 03:14:35 -0300

    parte dois de polimrfismo incompleta

---
+++ aula07/sobrecarga/Animal.java
@@ -0,0 +1,38 @@
+package aula07.sobrecarga;
+
+public abstract class Animal {
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    //Métodos
+    public abstract void  locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+
+    //Métodos especiais
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+}
---
+++ aula07/sobrecarga/Mamifero.java
@@ -0,0 +1,34 @@
+package aula07.sobrecarga;
+
+import aula07.Animal;
+
+public class Mamifero extends Animal {
+    private String corPelo;
+
+    //Métodos herdados
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamífero");
+    }
+
+    //Métodos especiais
+
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+}
